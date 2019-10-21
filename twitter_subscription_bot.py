cd95143d8ec1ea1a581c4e736c292c961eda3a2d
Yunzhi Gao <yunzhi@google.com>
2019-10-21 16:49:41 -0700

    fix not so obvious bugs

--- twitter_subscription_bot.py
+++ twitter_subscription_bot.py
@@ -17,35 +17,69 @@ START_MESSAGE = ('Command: `/subscribe_twitter twitter_user_link`. If you do thi
 with open('CREDENTIALS') as f:
 	CREDENTIALS = json.load(f)
 
-def loadSubscription():
-	try:
-		with open('SUBSCRIPTION') as f:
-			SUBSCRIPTION = json.load(f)
-	except:
-		SUBSCRIPTION = {}
+try:
+	with open('SUBSCRIPTION') as f:
+		SUBSCRIPTION = json.load(f)
+except:
+	SUBSCRIPTION = {}
 
 def saveSubscription():
 	with open('SUBSCRIPTION', 'w') as f:
-		f.write(json.dump(SUBSCRIPTION))
+		f.write(json.dumps(SUBSCRIPTION, sort_keys=True, indent=2))
 
 def getTwitterSubscription():
+	print(SUBSCRIPTION)
 	account_ids = set()
-	for _, subs in SUBSCRIPTION:
+	for subs in SUBSCRIPTION.values():
 		account_ids.update(subs.keys())
 	return account_ids
 
 def getSubscribers(tuid):
 	# if this bot does grow larger, we will need a relational db.
 	chat_ids = set()
-	for chat_id, subs in SUBSCRIPTION:
+	for chat_id, subs in SUBSCRIPTION.items():
 		if tuid in subs:
 			chat_ids.add(chat_id)
 	return chat_ids
 
+def getTContent(data):
+	if data['truncated']:
+		return data['extended_tweet']['full_text']
+	else:
+		return data['text']
+
+def getContent(data):
+	if data['retweeted_status']:
+		return getTContent(data['retweeted_status'])
+	else:
+		return getTContent(data)
+
+class TwitterListener(tweepy.StreamListener):
+	def on_data(self, data):
+		tweet_data = json.loads(data)
+		if tweet_data['in_reply_to_status_id_str']:
+			return
+		tuid = tweet_data['user']['id_str']
+		chat_ids = getSubscribers(tuid)
+		if not chat_ids:
+			return
+		for chat_id in chat_ids:
+			updater.bot.send_message(chat_id=chat_id, text=getContent(tweet_data))
+
+def twitterPushImp():
+	global twitterApi
+	global twitterStream
+	auth = tweepy.OAuthHandler(CREDENTIALS['twitter_consumer_key'], CREDENTIALS['twitter_consumer_secret'])
+	auth.set_access_token(CREDENTIALS['twitter_access_token'], CREDENTIALS['twitter_access_secret'])
+	twitterApi = tweepy.API(auth)
+	twitterListener = TwitterListener()
+	twitterStream = tweepy.Stream(auth=twitterApi.auth, listener=twitterListener)
+	twitterStream.filter(follow=getTwitterSubscription())
+
 def updateSubInfo(msg, bot):
 	try:
 		saveSubscription()
-		twitterStream.filter(follow=getTwitterSubscription())
+		twitterPushImp()
 		info = 'Twitter Subscription List: \n' +  '\n'.join(sorted(SUBSCRIPTION[str(msg.chat_id)].values()))
 		msg.reply_text(info, quote=False, parse_mode='Markdown', disable_web_page_preview=True)
 	except Exception as e:
@@ -62,9 +96,10 @@ def exportImp(msg, bot):
 	if not 'subscribe' in command:
 		msg.reply_text(START_MESSAGE, quote=False)
 		return
-	tuid, desc = getTwitterId(link)
+	tuid, desc = getTwitterUser(link)
 	if not tuid:
 		return
+	SUBSCRIPTION[str(msg.chat_id)] = SUBSCRIPTION.get(str(msg.chat_id), {})
 	subs = SUBSCRIPTION[str(msg.chat_id)]
 	if 'unsubscribe' in command:
 		if not subs or tuid not in subs:
@@ -99,48 +134,20 @@ dp = updater.dispatcher
 dp.add_handler(MessageHandler(Filters.command, export))
 dp.add_handler(MessageHandler(Filters.private, start))
 
-def getTContent(data):
-	if data['truncated']:
-		return data['extended_tweet']['full_text']
-	else:
-		return data['text']
-
-def getContent(data):
-	if data['retweeted_status']:
-		return getTContent(data['retweeted_status'])
-	else:
-		return getTContent(data)
-
-class TwitterListener(tweepy.StreamListener):
-	def on_data(self, data):
-		tweet_data = json.loads(data)
-		if tweet_data['in_reply_to_status_id_str']:
-			return
-		tuid = tweet_data['user']['id_str']
-		chat_ids = getSubscribers(tuid)
-		if not chat_ids:
-			return
-		for chat_id in chat_ids:
-			updater.bot.send_message(chat_id=chat_id, text=getContent(tweet_data))
-
-def twitter_push():
+def twitterPush():
 	while True:
 		try:
-			auth = tweepy.OAuthHandler(CREDENTIALS['twitter_consumer_key'], CREDENTIALS['twitter_consumer_secret'])
-			auth.set_access_token(CREDENTIALS['twitter_access_token'], CREDENTIALS['twitter_access_secret'])
-			twitterApi = tweepy.twitterApi(auth)
-			twitterListener = TwitterListener()
-			twitterStream = tweepy.Stream(auth=twitterApi.auth, listener=twitterListener)
-			twitterStream.filter(follow=getTwitterSubscription())
-		except Exception as e: #抓取到一段时间都会断开连接,睡眠一段时间再重新连接
-			time.sleep(251)
+			twitterPushImp()
+		except Exception as e:
+			time.sleep(30)
 			print(e)
+			tb.print_exc()
 
-t2 = threading.Thread(target=twitter_push)
+t2 = threading.Thread(target=twitterPush)
 t2.start()
+
+twitterPushImp()
 
 updater.start_polling()
 updater.idle()
-
-print('here----')
 
