a26299cb82e4b1ffa27c406ed1a5999499dd020c
Yunzhi Gao <yunzhi@google.com>
2019-10-21 15:25:18 -0700

    dev milestone 2

--- twitter_subscription_bot.py
+++ twitter_subscription_bot.py
@@ -28,25 +28,24 @@ def saveSubscription():
 	with open('SUBSCRIPTION', 'w') as f:
 		f.write(json.dump(SUBSCRIPTION))
 
+def getTwitterSubscription():
+	account_ids = set()
+	for _, ids in SUBSCRIPTION:
+		account_ids.update(ids)
+	return account_ids
+
 def trimURL(link):
 	if '://' not in link:
 		return link
 	loc = link.find('://')
 	return link[loc + 3:]
 
-def setInfo(msg, bot, link, soup):
+def updateSubInfo(msg, bot):
 	try:
 		if msg.from_user and msg.chat.id == msg.from_user.id:
 			return
-		info = 'All videos from Youtube playlist: ' + trimURL(link)
-		try:
-			bot.set_chat_description(msg.chat.id, info)
-		except:
-			pass
-		fist_reply = msg.reply_text(info, quote=False)
-		bot.pin_chat_message(msg.chat.id, fist_reply.message_id)
-		title = soup.find('h1', class_='pl-header-title').text
-		bot.set_chat_title(msg.chat.id, title)
+		info = 'Twitter Subscription List: \n' +  
+		msg.reply_text(info, quote=False)
 	except Exception as e:
 		print(e)
 		tb.print_exc()
@@ -54,7 +53,27 @@ def setInfo(msg, bot, link, soup):
 
 def exportImp(msg, bot):
 	command, link = msg.text.split()
+	if not 'subscribe' in command:
+		msg.reply_text(START_MESSAGE, quote=False)
+		return
+	tid = getTwitterId(link)
+	if not tid:
+		return
+	sub_list = SUBSCRIPTION[str(msg.chat_id)]
 	if 'unsubscribe' in command:
+		if not sub_list or str(tid) not in sublist:
+			return msg.reply_text('FAIL. No such subscription', quote=False)
+		sublist.remove(str(tid))
+		saveSubscription()
+		twitterStream.filter(follow=getTwitterSubscription())
+		updateSubInfo(msg, bot)
+		return
+	sub_list.append(str(tid))
+	saveSubscription()
+	twitterStream.filter(follow=getTwitterSubscription())
+	updateSubInfo(msg, bot)
+	return
+
 
 
 
@@ -82,28 +101,26 @@ dp = updater.dispatcher
 dp.add_handler(MessageHandler(Filters.command, export))
 dp.add_handler(MessageHandler(Filters.private, start))
 
-def telebot_poll(updater):
-	updater.start_polling()
-	updater.idle()
-
-t1 = threading.Thread(target=telebot_poll, arg=(updater,))
-t1.start()
-
 def twitter_push(updater):
 	while True:
 	    try:
-	        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	        auth.set_access_token(access_token, access_token_secret)
-	        api = tweepy.API(auth)
-	        print('start a new connection')
-	        myStreamListener = MyStreamListener()
-	        myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
-	        # 实时监测我们关注的这些推特自媒体账号
-	        myStream.filter(follow=accountIds)
+	        auth = tweepy.OAuthHandler(CREDENTIALS['twitter_consumer_key'], CREDENTIALS['twitter_consumer_secret'])
+	        auth.set_access_token(CREDENTIALS['twitter_access_token'], CREDENTIALS['twitter_access_secret'])
+	        twitterApi = tweepy.twitterApi(auth)
+	        twitterListener = TwitterListener()
+	        twitterStream = tweepy.Stream(auth=twitterApi.auth, listener=twitterListener)
+	        twitterStream.filter(follow=getTwitterSubscription())
 	    except Exception as e: #抓取到一段时间都会断开连接,睡眠一段时间再重新连接
 	        time.sleep(251)
 	        print(e)
 
 t2 = threading.Thread(target=twitter_push, arg=(updater,))
 t2.start()
+
+def telebot_poll(updater):
+	updater.start_polling()
+	updater.idle()
+
+t1 = threading.Thread(target=telebot_poll, arg=(updater,))
+t1.start()
 
