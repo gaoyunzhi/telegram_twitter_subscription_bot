65c5c058ca2ce20937b309198d4aa355798ac950
Yunzhi Gao <yunzhi@google.com>
2019-10-21 16:13:21 -0700

    fix obvious bugs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import json
 
 REQUIRED_KEYS = set(['bot_token', 'twitter_consumer_key', 'twitter_consumer_secret', 'twitter_access_token', 'twitter_access_secret'])
 
-def setup(is_reload = False):
+def setup(arg = ''):
 	RUN_COMMAND = 'nohup python3 twitter_subscription_bot.py &'
 
 	CREDENTIALS = {}
@@ -19,7 +19,7 @@ def setup(is_reload = False):
 			print('ERROR: please fill the CREDENTIALS file in json format, required keys : ' + ', '.join(sorted(REQUIRED_KEYS)))
 			return
 
-	if not is_reload:
+	if arg != 'reload' and arg != 'debug':
 		os.system('curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py')
 		os.system('python3 get-pip.py')
 		os.system('rm get-pip.py')
@@ -28,12 +28,16 @@ def setup(is_reload = False):
 		os.system('pip3 install python-telegram-bot --upgrade') # need to use some experiement feature, e.g. message filtering
 			
 	# kill the old running bot if any. If you need two same bot running in one machine, use mannual command instead
-	os.system('ps aux | grep python | grep twitter_subscription_bot | awk '{print $2}' | xargs kill -9')
-	return os.system(RUN_COMMAND)
+	os.system("ps aux | grep python | grep twitter_subscription_bot | awk '{print $2}' | xargs kill -9")
+
+	if arg == 'debug':
+		os.system(RUN_COMMAND[6:-2])
+	else:
+		os.system(RUN_COMMAND)
 
 
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
-		setup(sys.argv[1] == 'reload')
+		setup(sys.argv[1])
 	else:
 		setup('')
--- twitter_subscription_bot.py
+++ twitter_subscription_bot.py
@@ -8,8 +8,8 @@ import traceback as tb
 import json
 import tweepy
 import threading
+import time
 
-DEBUG_GROUP = -1001198682178  # @bot_debug
 START_MESSAGE = ('Command: `/subscribe_twitter twitter_user_link`. If you do this inside group/channel, please '
 								 'add this bot as admin.')
 
@@ -55,7 +55,7 @@ def updateSubInfo(msg, bot):
 def getTwitterUser(link):
 	screenname = [x for x in link.split('/') if x][-1]
 	user = twitterApi.get_user(screenname)
-	return str(user.id), ''[' + user.name + '](twitter.com/' + str(user.screen_name) + ')''
+	return str(user.id), '[' + user.name + '](twitter.com/' + str(user.screen_name) + ')'
 
 def exportImp(msg, bot):
 	command, link = msg.text.split()
@@ -101,48 +101,46 @@ dp.add_handler(MessageHandler(Filters.private, start))
 
 def getTContent(data):
 	if data['truncated']:
-        return data['extended_tweet']['full_text']
-    else:
-        return data['text']
+		return data['extended_tweet']['full_text']
+	else:
+		return data['text']
 
 def getContent(data):
-    if data['retweeted_status']:
-    	return getTContent(data['retweeted_status'])
-    else:
-    	return getTContent(data)
+	if data['retweeted_status']:
+		return getTContent(data['retweeted_status'])
+	else:
+		return getTContent(data)
 
 class TwitterListener(tweepy.StreamListener):
-    def on_data(self, data):
-        tweet_data = json.loads(data)
-        if tweet_data['in_reply_to_status_id_str']:
-            return
-        tuid = tweet_data['user']['id_str']
-        chat_ids = getSubscribers(tuid)
-        if not chat_ids:
-        	return
-        for chat_id in chat_ids:
-        	updater.bot.send_message(chat_id=chat_id, text=getContent(tweet_data))
+	def on_data(self, data):
+		tweet_data = json.loads(data)
+		if tweet_data['in_reply_to_status_id_str']:
+			return
+		tuid = tweet_data['user']['id_str']
+		chat_ids = getSubscribers(tuid)
+		if not chat_ids:
+			return
+		for chat_id in chat_ids:
+			updater.bot.send_message(chat_id=chat_id, text=getContent(tweet_data))
 
-def twitter_push(updater):
+def twitter_push():
 	while True:
-	    try:
-	        auth = tweepy.OAuthHandler(CREDENTIALS['twitter_consumer_key'], CREDENTIALS['twitter_consumer_secret'])
-	        auth.set_access_token(CREDENTIALS['twitter_access_token'], CREDENTIALS['twitter_access_secret'])
-	        twitterApi = tweepy.twitterApi(auth)
-	        twitterListener = TwitterListener()
-	        twitterStream = tweepy.Stream(auth=twitterApi.auth, listener=twitterListener)
-	        twitterStream.filter(follow=getTwitterSubscription())
-	    except Exception as e: #抓取到一段时间都会断开连接,睡眠一段时间再重新连接
-	        time.sleep(251)
-	        print(e)
+		try:
+			auth = tweepy.OAuthHandler(CREDENTIALS['twitter_consumer_key'], CREDENTIALS['twitter_consumer_secret'])
+			auth.set_access_token(CREDENTIALS['twitter_access_token'], CREDENTIALS['twitter_access_secret'])
+			twitterApi = tweepy.twitterApi(auth)
+			twitterListener = TwitterListener()
+			twitterStream = tweepy.Stream(auth=twitterApi.auth, listener=twitterListener)
+			twitterStream.filter(follow=getTwitterSubscription())
+		except Exception as e: #抓取到一段时间都会断开连接,睡眠一段时间再重新连接
+			time.sleep(251)
+			print(e)
 
-t2 = threading.Thread(target=twitter_push, arg=(updater,))
+t2 = threading.Thread(target=twitter_push)
 t2.start()
 
-def telebot_poll(updater):
-	updater.start_polling()
-	updater.idle()
+updater.start_polling()
+updater.idle()
 
-t1 = threading.Thread(target=telebot_poll, arg=(updater,))
-t1.start()
+print('here----')
 
